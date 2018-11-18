389be4e1df4a6cc9c189f8e7f6b589041aa2d345
Simon Andrews <me@simonandrews.ca>
2018-11-18 19:10:47 +0000

    feat: add viewport sizes to theme

--- packages/components/src/theme/theme.js
+++ packages/components/src/theme/theme.js
@@ -3,11 +3,13 @@ import color from "./color";
 import flourish from "./flourish";
 import space from "./space";
 import type from "./type";
+import viewportSizes from "./viewportSizes";
 
 export default {
   animation,
   color,
   flourish,
   space,
-  type
+  type,
+  viewportSizes
 };
