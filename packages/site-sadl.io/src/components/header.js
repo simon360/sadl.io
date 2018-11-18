0b2b775acc4e6527a87a7ae16396f6167a75ab8d
Simon Andrews <me@simonandrews.ca>
2018-11-18 22:48:07 +0000

    feat: update site to use new Layout and Section components

--- packages/site-sadl.io/src/components/header.js
+++ packages/site-sadl.io/src/components/header.js
@@ -1,33 +1,26 @@
 import React from "react";
 import { Link } from "gatsby";
 
-import Cutter from "@sadl/components/components/Cutter";
+import { Container, Grid, GridItemCentered } from "@sadl/components/components/Layout";
 import Logo from "@sadl/components/components/Logo";
 import Section from "@sadl/components/components/Section";
 import theme from "@sadl/components/theme";
 
 const Header = ({ siteTitle }) => (
   <>
-    <div
-      style={{
-        background: theme.color.primary
-      }}
-    >
-      <Section>
-        <h1 style={{ margin: 0 }}>
-          <Link
-            to="/"
-            style={{
-              color: "white",
-              textDecoration: "none"
-            }}
-          >
-            <Logo height={theme.space.two} title={siteTitle} />
-          </Link>
-        </h1>
-      </Section>
-    </div>
-    <Cutter bottomColor="white" topColor={theme.color.primary} />
+    <Section cutter mode="dark">
+      <Container>
+        <Grid>
+          <GridItemCentered>
+            <h1>
+              <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+                <Logo height={theme.space.two} title={siteTitle} />
+              </Link>
+            </h1>
+          </GridItemCentered>
+        </Grid>
+      </Container>
+    </Section>
   </>
 );
 
--- packages/site-sadl.io/src/pages/index.js
+++ packages/site-sadl.io/src/pages/index.js
@@ -2,18 +2,65 @@ import React from "react";
 
 import Layout from "../components/layout";
 
-import { BiggerHeading } from "@sadl/components/components/Heading";
+import {
+  BigHeading,
+  BiggerHeading,
+  VeryBigHeading
+} from "@sadl/components/components/Heading";
+import {
+  Container,
+  Grid,
+  GridItem14,
+  GridItem34,
+  GridItemFull
+} from "@sadl/components/components/Layout";
 import Link from "@sadl/components/components/Link";
+import Section from "@sadl/components/components/Section";
 import Text from "@sadl/components/components/Text";
 
 const IndexPage = () => (
   <Layout>
-    <BiggerHeading>Simon Andrews Development Limited</BiggerHeading>
-    <Text>
-      SADL is a registered limited company in England and Wales (10271125). You can get in touch
-      at <Link href="mailto:hello@sadl.io">hello@sadl.io</Link>.
-    </Text>
-    <Text><Link href="/system">View the SADL design system</Link></Text>
+    <Section>
+      <Container>
+        <Grid>
+          <GridItemFull>
+            <VeryBigHeading>Simon Andrews Development Limited</VeryBigHeading>
+          </GridItemFull>
+          <GridItem34>
+            <BiggerHeading>About SADL</BiggerHeading>
+            <Text>
+              SADL is focused on making the Web better. Using cutting-edge tools
+              where they help, tried-and-true solutions where they work, and a
+              healthy dose of creaticity, SADL can deliver a better experience
+              for your customers and your develoeprs.
+            </Text>
+            <Text>
+              SADL is a registered limited company in England and Wales
+              (10271125).
+            </Text>
+            <Text>
+              <Link href="/system">View the SADL design system</Link>
+            </Text>
+          </GridItem34>
+          <GridItem14>
+            <BiggerHeading>Find SADL</BiggerHeading>
+            <ul>
+              <li>
+                GitHub:{" "}
+                <Link href="https://github.com/simon360">@simon360</Link>
+              </li>
+              <li>
+                Twitter:{" "}
+                <Link href="https://twitter.com/simon360">@simon360</Link>
+              </li>
+              <li>
+                Email: <Link href="mailto:hello@sadl.io">hello@sadl.io</Link>
+              </li>
+            </ul>
+          </GridItem14>
+        </Grid>
+      </Container>
+    </Section>
   </Layout>
 );
 
