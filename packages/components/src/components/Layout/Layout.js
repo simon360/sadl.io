7da78269194c1e3267d8355a602a98877c68b8b6
Simon Andrews <me@simonandrews.ca>
2019-04-28 19:21:34 +0100

    fix: replace styled-components with @emotion/styled in Layout

--- packages/components/src/components/Layout/Layout.js
+++ packages/components/src/components/Layout/Layout.js
@@ -1,30 +1,33 @@
-import styled, { css } from "styled-components";
+import { css } from "@emotion/core";
+import styled from "@emotion/styled";
+
+import theme from "@sadl/components/theme";
 
 // Max width is 48 spacing units, minus one half on each side.
 const maxWidth = css`
-  max-width: calc(47 * ${props => props.theme.space.one});
+  max-width: calc(47 * ${theme.space.one});
 `;
 
 export const Container = styled.div`
   margin: 0 auto;
   ${maxWidth}
-  padding-bottom: ${props => props.theme.space.one};
-  padding-left: ${props => props.theme.space.half};
-  padding-right: ${props => props.theme.space.half};
-  padding-top: ${props => props.theme.space.one};
+  padding-bottom: ${theme.space.one};
+  padding-left: ${theme.space.half};
+  padding-right: ${theme.space.half};
+  padding-top: ${theme.space.one};
 `;
 
 export const Grid = styled.div`
-  column-gap: ${props => props.theme.space.half};
+  column-gap: ${theme.space.half};
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  row-gap: ${props => props.theme.space.one};
+  row-gap: ${theme.space.one};
 
-  @media (min-width: ${props => props.theme.viewportSizes.small}) {
+  @media (min-width: ${theme.viewportSizes.small}) {
     grid-template-columns: repeat(6, 1fr);
   }
 
-  @media (min-width: ${props => props.theme.viewportSizes.medium}) {
+  @media (min-width: ${theme.viewportSizes.medium}) {
     grid-template-columns: repeat(12, 1fr);
   }
 `;
@@ -32,7 +35,7 @@ export const Grid = styled.div`
 export const GridItem12 = styled.div`
   grid-column: span 3;
 
-  @media (min-width: ${props => props.theme.viewportSizes.medium}) {
+  @media (min-width: ${theme.viewportSizes.medium}) {
     grid-column: span 6;
   }
 `;
@@ -40,11 +43,11 @@ export const GridItem12 = styled.div`
 export const GridItem14 = styled.div`
   grid-column: span 3;
 
-  @media (min-width: ${props => props.theme.viewportSizes.small}) {
+  @media (min-width: ${theme.viewportSizes.small}) {
     grid-column: span 6;
   }
 
-  @media (min-width: ${props => props.theme.viewportSizes.medium}) {
+  @media (min-width: ${theme.viewportSizes.medium}) {
     grid-column: span 3;
   }
 `;
@@ -52,11 +55,11 @@ export const GridItem14 = styled.div`
 export const GridItem34 = styled.div`
   grid-column: span 3;
 
-  @media (min-width: ${props => props.theme.viewportSizes.small}) {
+  @media (min-width: ${theme.viewportSizes.small}) {
     grid-column: span 6;
   }
 
-  @media (min-width: ${props => props.theme.viewportSizes.medium}) {
+  @media (min-width: ${theme.viewportSizes.medium}) {
     grid-column: span 9;
   }
 `;
@@ -64,11 +67,11 @@ export const GridItem34 = styled.div`
 export const GridItemFull = styled.div`
   grid-column: span 3;
 
-  @media (min-width: ${props => props.theme.viewportSizes.small}) {
+  @media (min-width: ${theme.viewportSizes.small}) {
     grid-column: span 6;
   }
 
-  @media (min-width: ${props => props.theme.viewportSizes.medium}) {
+  @media (min-width: ${theme.viewportSizes.medium}) {
     grid-column: span 12;
   }
 `;
@@ -82,16 +85,16 @@ export const GridItemCentered = styled(GridItemFull)`
 export const GridItemInset = styled.div`
   grid-column: span 3;
 
-  @media (min-width: ${props => props.theme.viewportSizes.small}) {
+  @media (min-width: ${theme.viewportSizes.small}) {
     grid-column: 2 / 6;
   }
 
-  @media (min-width: ${props => props.theme.viewportSizes.medium}) {
+  @media (min-width: ${theme.viewportSizes.medium}) {
     grid-column: 3 / 11;
   }
 `;
 
 export const Stack = styled.div`
   display: grid;
-  row-gap: ${props => props.theme.space.one};
+  row-gap: ${theme.space.one};
 `;
