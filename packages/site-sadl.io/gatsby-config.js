d14caf603920002635c25966ff9764615731caf3
Simon Andrews <me@simonandrews.ca>
2019-04-28 21:38:17 +0100

    chore: expunge styled-components

--- packages/site-sadl.io/gatsby-config.js
+++ packages/site-sadl.io/gatsby-config.js
@@ -25,7 +25,6 @@ module.exports = {
         icon: "src/images/gatsby-icon.png" // This path is relative to the root of the site.
       }
     },
-    "gatsby-plugin-styled-components",
     "gatsby-plugin-emotion"
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
